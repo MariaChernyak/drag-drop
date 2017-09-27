b920e1674456cc73a261516d305be6269b4225d6
MariaChernyak <mchernyak100@gmail.com>
2017-09-27 12:27:07 +0300

    update

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 var dropContainer = document.querySelector('.drop-container');
+var dragElements = document.querySelectorAll('.drag-container img');
+var element;
 
+function handleDragStart(e){
+	element = e.target;
+	console.log(e);
+}
 function handleDragEnter(e) {
     this.classList.add('over');
 }
-
 function handleDragLeave(e){
 	this.classList.remove('over');
 }
 
 function handleDrop(e){
-	console.log(new Date())
+	e.preventDefault();
+	dropContainer.appendChild(element);
+}
+function handleDragEnd(){
+	dropContainer.classList.remove('over');
+}
+function handleDragOver(e){
 	e.preventDefault();
 }
 
 dropContainer.addEventListener('dragenter', handleDragEnter, false);
 dropContainer.addEventListener('dragleave', handleDragLeave, false);
 dropContainer.addEventListener('drop', handleDrop, false);
-document.querySelector('.drag-container img').addEventListener('drop', handleDrop, false);
+dropContainer.addEventListener('dragover', handleDragOver, false);
+
+var length = dragElements.length;
+for(var i = 0; i < length; i++){
+	dragElements[i].addEventListener('dragstart', handleDragStart, false);
+	dragElements[i].addEventListener('dragend', handleDragEnd, false);
+}
+
